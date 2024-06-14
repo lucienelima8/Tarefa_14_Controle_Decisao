6ae601d083b4520dec19b54e70d737c815618041
Luciene <lucienedelima8@gmail.com>
2024-06-14 20:10:01 -0300

    iniciando a segunda div

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/*Média aritmética*/
+/*MÉDIA ARITMÉTICA DE 3 NÚMEROS*/
+
 /*Usuáiro digitar 3 números e o JS fazer a média aritmética*/
 
 function calcular_media () {
@@ -9,5 +10,21 @@ function calcular_media () {
     const media = (num1 + num2 + num3) / 3;
 
     document.getElementById('resultArea').innerHTML = `A média é: ` + media;
+
+}
+
+/*USUÁRIO É MENOR DE IDADE OU NÃO*/
+
+function calcular_idade {
+
+    const idade = parseFloat(document.getElementById('idade').value);
+
+    if(idade >= 18) {
+    alert("Vocé é maior de idade");
+
+    }else {
+    alert("Você é menor de idade");
+
+    }
 
 }
